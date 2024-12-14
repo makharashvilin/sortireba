d554b6010c77708c4406c264262ac87729c9cbee
makharashvilin <163711814+makharashvilin@users.noreply.github.com>
2024-12-14 23:12:08 +0400

    Add files via upload

---
+++ mziurishop/products/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import Product, Category
+
+def home(request):
+
+    filters = dict()
+    product_name = request.GET.get('product_name')
+    if product_name:
+        filters['name__icontains'] = product_name
+    min_price = request.GET.get('min_price')
+    if min_price:
+        filters['price__gt'] =  min_price
+    max_price = request.GET.get('max_price')
+    if max_price:
+        filters['price__lt'] =  max_price
+    address = request.GET.get('address')
+    if address:
+        filters['address__icontains'] = address
+    category = request.GET.get('category')
+    if category:
+        filters['category_id'] = category
+    products = Product.objects.filter(**filters)
+    sort_by = request.GET.get('sort')
+    if sort_by:
+        products = products.order_by(sort_by)
+    categories = Category.objects.all()
+
+
+    return render(request, 'home.html', context={'products': products, 'categories':categories})
+
+
+def product_detail(request):
+    return render(request, 'product_detail.html')
